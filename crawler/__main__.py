0d3a3bd487cec31828f3308c4057a501be13a292
ronbarrantes <ronbarrantes@gmail.com>
2021-04-21 01:40:22 -0700

    Sort the incoming info

--- crawler/__main__.py
+++ crawler/__main__.py
@@ -1,24 +1,21 @@
 import urllib.request
 from wikicrawler import WikiCrawler
+from collections import OrderedDict
+
+
+
 
 url = urllib.request.urlopen('https://en.wikipedia.org/wiki/Microsoft')
 html = url.read().decode()
 url.close()
 section = "History"
+frequency = 10
 
 crawl = WikiCrawler()
 crawl.section = section
 crawl.feed(html)
 
+ordered_and_trim_list = OrderedDict(sorted(crawl.data.items(), key=lambda x: x[1], reverse=True)[:frequency])
 
-
-# DELETE ALL THE STUFF ON THE BOTTOM
-
-f = open('./dump.txt', 'w')
-f.write(str(crawl.data))
-
-
-
-
-
-print('all done')
+for k, v in ordered_and_trim_list.items():
+	print(k,v)
--- crawler/wikicrawler.py
+++ crawler/wikicrawler.py
@@ -5,7 +5,7 @@ class WikiCrawler(HTMLParser):
 	def __init__(self):
 		HTMLParser.__init__(self)
 		self.should_record = False
-		self.data = []
+		self.data = {}
 		self.section = ""
 
 	def handle_starttag(self, tag, attrs):
@@ -23,4 +23,7 @@ class WikiCrawler(HTMLParser):
 			for i in range(len(temp_data)):
 				word = temp_data[i]
 				if re.match('[a-zA-Z]', word):
-					self.data.append(word)
+					if word in self.data.keys():
+						self.data[word] = self.data[word] + 1
+					else: 
+						self.data[word] = + 1
