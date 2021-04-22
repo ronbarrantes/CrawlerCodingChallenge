3e5aa9391cbe70228af01886afde900f188c2320
ronbarrantes <ronbarrantes@gmail.com>
2021-04-21 17:11:24 -0700

    Add the limit input

--- crawler/__main__.py
+++ crawler/__main__.py
@@ -6,13 +6,50 @@ url = urllib.request.urlopen('https://en.wikipedia.org/wiki/Microsoft')
 html = url.read().decode()
 url.close()
 section = "History"
-frequency = 10
+default_display_count = 10
+exclude_words = []
+
+# get the first input and verify it's an int
+display_count = input('Set a word limit: (Default: 10)\n')
+while True:
+	try:
+		if len(display_count) < 0:
+			display_count = default_display_count
+			break
+
+		if len(display_count) >= 0:
+			display_count = int(display_count)
+			break
+
+	except:
+		display_count = input('Please add a valid number:\n')
+
+
+
+print(int(display_count))
+display_count = (default_display_count, display_count)[display_count >= 0]
+
+
+
+
+
+
+
+
+
+
+
+# get the second input and verify it's a string
+
+
 
 crawl = WikiCrawler()
 crawl.section = section
 crawl.feed(html)
 
-ordered_and_trim_list = OrderedDict(sorted(crawl.data.items(), key=lambda x: x[1], reverse=True)[:frequency])
+# filtered_items = 
+
+ordered_and_trim_list = OrderedDict(sorted(crawl.data.items(), key=lambda x: x[1], reverse=True)[:display_count])
 
 for key, val in ordered_and_trim_list.items():
-	print(f"{key:>9}: {val}")
+	print(f"{key:>11}: {val}")
